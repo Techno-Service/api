36d02484dab9d66c522a7640eb3679e3bfabb206
Amr <muhammad.amro.moussa@gmail.com>
2019-11-09 11:02:13 +0200

    Topology

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const config = require('../config')
 
 module.exports = function() {
 	const db = config.db
-	 // mongoose.set('useUnifiedTopology', true)
+	 mongoose.set('useUnifiedTopology', true)
 	mongoose.set('useNewUrlParser', true)
 	mongoose.set('useCreateIndex', true)
 	mongoose.set('useFindAndModify', false)
