df3cd3408fe5f642bf8e59ff089f60a62c174cdf
Amr <muhammad.amro.moussa@gmail.com>
2019-11-09 13:04:39 +0200

    Cors trial

--- startup/routes.js
+++ startup/routes.js
@@ -10,7 +10,16 @@ const move = require('../routes/move')
 const locations = require('../routes/locations')
 const error = require('../middleware/error')
 const cors = require('cors')
-
+const whitelist = ['*', 'https://8080-dot-9728892-dot-devshell.appspot.com']
+var corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	}
+}
 
 // const formData = require('express-form-data')
 // const os = require('os')
@@ -18,7 +27,7 @@ const cors = require('cors')
 
 
 module.exports = function(app) {
-	app.use(cors())
+	app.use(cors(corsOptions))
 	app.use(express.json())
 	app.use( bodyParser.json() )       // to support JSON-encoded bodies
 	app.use(bodyParser.urlencoded({
